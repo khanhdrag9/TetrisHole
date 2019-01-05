c017ecd1d10425b52367e005e1d2a051d4e2c672
khanhdrag9 <tkhanh783@gmail.com>
2019-01-05 21:44:34 +0700

    + GObjectManager : create and handle objects

---
+++ Classes/GObjectManager.h
@@ -0,0 +1,21 @@
+#include "Header.h"
+
+class Circle;
+class Container;
+class gObject;
+
+class GObjectManager
+{
+    shared_ptr<Circle> _releaseCir;
+    
+public:
+    GObjectManager();
+    virtual ~GObjectManager();
+    
+    void init();
+    
+    shared_ptr<Circle> createCircle();
+    void setPositionForObject(shared_ptr<gObject>& obj, Vec2& pos);
+    void setPositionForContainer(shared_ptr<Container>& ctn, Vec2& pos);
+    
+};
